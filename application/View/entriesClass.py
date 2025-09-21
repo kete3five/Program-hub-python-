3912b3b0d250291d56fa24de2200d3d6814e5ab5
kete3five <vladpochinok2010@gmail.com>
2025-09-21 17:49:06 +0300

    added alot of stuff

--- application/View/entriesClass.py
+++ application/View/entriesClass.py
@@ -1,11 +1,7 @@
 import tkinter as tk
-import functionToPlot
-import graphfile
 
-class functionTPFrame(tk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
-        self.functions = []
+class graphEntries:
+    def __init__(self):
         self.nameText = tk.Label(self, text="Please enter the function you would like to visualize")
         self.nameText.pack(pady=5)
         self.task_entryFunc = tk.Entry(self, width=25,font=("Arial",12,))
@@ -26,17 +22,3 @@ class functionTPFrame(tk.Frame):
         self.nameName.pack(pady=5)
         self.task_entryName = tk.Entry(self, width=25,font=("Arial",12,))
         self.task_entryName.pack(pady=10)
-        self.task_button = tk.Button(self, text="add to list",command=lambda: self.sendToList()).pack(pady=10)
-        self.task_visButton = tk.Button(self, text="visualize the Functions",command=lambda: self.visualize()).pack(pady=10)
-    def getFunction(self):
-        task = self.task_entryFunc.get().strip()
-        return task
-    def sendToList(self):
-        #ftp = functionToPlot.functionToPlot()
-        #ftp.renderGraph(int(self.task_entryRange.get().strip()))
-        #ftp.renderGraph(int(self.task_entryRange.get().strip()), ["sin(x)","cos(x)"])
-        self.functions.append(graphfile.graph(self.task_entryColor.get().strip(), self.task_entrywidth.get().strip(), self.task_entryName.get().strip(), self.task_entryFunc.get().strip()))
-        print(len(self.functions))
-    def visualize(self):
-        ftp = functionToPlot.functionToPlot()
-        ftp.renderGraph(int(self.task_entryRange.get()), self.functions)
---
+++ application/View/graphfile.py
@@ -0,0 +1,6 @@
+class graph:
+    def __init__(self, colour, width, name, equation):
+        self.colour = colour
+        self.width = width
+        self.name = name
+        self.equation = equation
