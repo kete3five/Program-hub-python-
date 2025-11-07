d93fb942abd0cf539074be22400204959b16e84f
kete3five <vladpochinok2010@gmail.com>
2025-11-07 18:15:59 +0200

    fixed issues with pyline

--- application/Controller/button_factory.py
+++ application/Controller/button_factory.py
@@ -2,9 +2,9 @@ import tkinter as tk
 import subprocess
 
 class ButtonFactory:
-    def buttonBuild(self, root, details_button):
+    def buttonBuild(self, root, db):
         """initializes a button"""
-        return tk.Button(root, text=details_button.name, width=10, command=self.openSP(details_button.path))
+        return tk.Button(root, text=db.name, width=10, command=self.openSP(details_button.path))
     def openSP(self, details):
         """opens a subprocess"""
         subprocess.Popen(["python", details])
--- application/View/add_projects.py
+++ application/View/add_projects.py
@@ -7,6 +7,7 @@ import dataSaver
 
 
 class addProject(tk.Frame):
+    """adding projects to the project"""
     def __init__(self, controller, parent):
         super().__init__(parent)
         self.dt = dataSaver.dataSaver()
@@ -22,15 +23,15 @@ class addProject(tk.Frame):
         self.pathText.pack(pady=5)
         self.task_entryPath.pack(pady=10)
         tk.Button(self,text="create a button",command=lambda: self.addTask()).pack(pady=10)
-        tk.Button(self,text="back",command=lambda: controller.show_frame("Home")).pack(pady=10)  
-        tk.Button(self,text="Delete Button", comman=lambda: self.deleteTask()).pack(pady=10) 
+        tk.Button(self,text="back",command=lambda: controller.show_frame("Home")).pack(pady=10)
+        tk.Button(self,text="Delete Button", comman=lambda: self.deleteTask()).pack(pady=10)
         self.task_list=tk.Listbox(self,width=40,height=10,selectmode=tk.SINGLE)
         tk.Button(self, text="change button", command=lambda: self.changeButton()).pack(pady=10)
         self.task_list.pack(pady=5)
         self.task_list.bind("<<ListboxSelect>>", self.takeFromList)
         self.displayTasks()
     #adding the tasks
-    def addTask(self):
+    def add_task(self):
         task = self.task_entryName.get().strip()
         task2 = self.task_entryPath.get().strip()
         if task and task2:
@@ -39,7 +40,7 @@ class addProject(tk.Frame):
         else:
             messagebox.showwarning("Please enter text in both fields.")
     #deleting the tasks
-    def deleteTask(self):
+    def delete_task(self):
         #todelete = self.task_entryName.get()
         try:
             selected = self.task_list.curselection()[0]
@@ -48,18 +49,18 @@ class addProject(tk.Frame):
         except IndexError:
             messagebox.showwarning("Warning","please enter a task.")
     #displaying tasks
-    def displayTasks(self):
+    def display_tasks(self):
         for i in self.buttons1:
             self.task_list.insert(tk.END, i.name)
     #change the button
-    def changeButton(self):
+    def change_button(self):
         taskName = self.task_entryName.get().strip()
         taskPath = self.task_entryPath.get().strip()
         line = self.task_list.curselection()[0]
         self.dt.changeFile(taskName, taskPath, line)
         messagebox.showinfo("The task has successfully been changed")
     #puts information into entry fields from the list
-    def takeFromList(self, amount):
+    def take_from_list(self, amount):
         selected = self.task_list.curselection()[0]
         self.task_entryName.insert(0, self.buttons1[selected].name)
         self.task_entryPath.insert(0, self.buttons1[selected].path)
